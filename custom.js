c58be9a6555101f3b02d42fffb262a43b8060e33
Hari Krishna S <hari@securden.com>
2025-04-07 12:28:15 +0530

    - Added the buttons with the correct questions - Enabled the js in a dynamic way to take the questions and display the answer on its own with the id

---
+++ custom.js
@@ -0,0 +1,60 @@
+const questions = [
+
+    {
+        question: "Which is the largest animal in the world",
+        answers : [
+
+            {
+                text: "rat", correct:"false"
+
+            },
+            { text: "cow", correct:"false"},
+            { text: "Shark", correct:"true"},
+            { text: "Buffalo", correct:"false"}
+        ]
+
+    },
+    {
+        question: "Who is the biggest player in IPL",
+        answers:[
+            {text: "Virat Kohli", correct: "false"},
+            {text: "MSD", correct: "true"},
+            {text: "Rohit Sharma", correct:"false"},
+            {text: "Suresh Raina", correct:"false"}
+        ]
+    }
+
+
+];
+
+const questionElement = document.getElementById("question");
+const answerElement = document.getElementById("answer-buttons");
+const nextElement = document.getElementById("next-btn");
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+
+    nextElement.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answers =>{
+        const button = document.createElement("button");
+        button.innerHTML = answers.text;
+        button.classList.add("btn");
+        answerElement.appendChild(button);
+    })
+}
+
+startQuiz();
